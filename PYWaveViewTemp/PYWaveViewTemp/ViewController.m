3a1d8b41c9256715e737a004de5d0692db46de4d
liPengYue <lipengyue@5lux.com>
2017-04-27 00:22:19 +0800

    no message

--- PYWaveViewTemp/PYWaveViewTemp/ViewController.m
+++ PYWaveViewTemp/PYWaveViewTemp/ViewController.m
@@ -12,14 +12,28 @@
 @property (nonatomic,weak) PYWaveView *waveView;
 //停止按钮的最大y值
 @property (nonatomic,assign) CGFloat stopWaveViewMaxY;
+//储存颜色的数组
+@property (nonatomic,strong) NSArray<UIColor *>*colorArray;
+@property (nonatomic,assign) NSInteger colorCount;
 @end
 
 @implementation ViewController
 
+- (NSArray<UIColor *> *)colorArray{
+    if (!_colorArray) {
+        UIColor *color1 = [UIColor colorWithRed:10/255.0 green:50/255.0 blue:255/255.0 alpha:0.20f];
+        UIColor *color2 = [UIColor colorWithRed:20/255.0 green:100/255.0 blue:255/255.0 alpha:0.40f];
+        UIColor *color3 = [UIColor colorWithRed:30/255.0 green:150/255.0 blue:255/255.0 alpha:0.60f];
+        UIColor *color4 = [UIColor colorWithRed:60/255.0 green:200/255.0 blue:255/255.0 alpha:0.80f];
+        UIColor *color5 = [UIColor colorWithRed:150/255.0 green:255/255.0 blue:255/255.0 alpha:.60f];
+        _colorArray = @[color1,color2,color3,color4,color5];
+    }
+    return _colorArray;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor colorWithWhite:.9 alpha:.8];
-    
     //设置冲浪视图
     [self setupWaceView];
     
@@ -28,6 +42,9 @@
     
     //改变形状按钮
     [self setupWaceViewShape];
+    
+    //改变颜色数量
+    [self setupAddColor];
 }
 
 
@@ -45,7 +62,7 @@
     //进度（从下到上占得self.Height的比例）
     waveView.progress = 0.5;
     //颜色数组
-    waveView.colorMutableArray = [self creatColorSet];
+    waveView.colorMutableArray = [NSMutableArray arrayWithObject:self.colorArray.firstObject];
     //北京颜色数组
     waveView.waveViewBackgroundColor = [UIColor colorWithRed:0.9 green:0.8 blue:0.9 alpha:0.4];
     //色块之间横向的距离
@@ -63,15 +80,6 @@
     waveView.isWaveStart = true;
 }
 
-//获取颜色数组
-- (NSMutableArray <UIColor *>*)creatColorSet {
-    UIColor *color1 = [UIColor colorWithRed:10/255.0 green:50/255.0 blue:255/255.0 alpha:0.20f];
-    UIColor *color2 = [UIColor colorWithRed:20/255.0 green:100/255.0 blue:255/255.0 alpha:0.40f];
-    UIColor *color3 = [UIColor colorWithRed:30/255.0 green:150/255.0 blue:255/255.0 alpha:0.60f];
-    UIColor *color4 = [UIColor colorWithRed:60/255.0 green:200/255.0 blue:255/255.0 alpha:0.80f];
-    UIColor *color5 = [UIColor colorWithRed:150/255.0 green:255/255.0 blue:255/255.0 alpha:.60f];
-    return @[color1,color2,color3,color4,color5].mutableCopy;
-}
 
 
 //MARK: 停止冲浪
@@ -128,6 +136,28 @@
     }else{
         [self setupPath];
     }
+}
+
+
+#pragma mark - 改变颜色数量
+- (void)setupAddColor{
+    UIButton *button = [[UIButton alloc]init];
+    button.center = CGPointMake(self.view.center.x, self.stopWaveViewMaxY + 100);
+    button.bounds = CGRectMake(0, 0, 200, 50);
+    [self.view addSubview:button];
+    [button addTarget:self action:@selector(clickAddColorButton:) forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"点击我添加颜色" forState:UIControlStateNormal];
+    
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+}
+
+- (void)clickAddColorButton: (UIButton *)button {
+    if (self.colorCount >= self.colorArray.count) {
+        self.colorCount = 0;
+    }
+    [self.waveView.colorMutableArray addObject:self.colorArray[self.colorCount]];
+    self.colorCount ++;
+    
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
